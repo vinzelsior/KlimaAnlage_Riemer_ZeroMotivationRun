a15eaf66f55e41d3ae9d957c228aebe183ff5891
Cedric Zwahlen <cedric-vince@gmx.ch>
2020-02-25 16:12:35 +0100

    Initial Commit

---
+++ KlimaanlageRiemer_ZeroMotivationRun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KlimaanlageRiemer_ZeroMotivationRun
+//
+//  Created by Cedric Zwahlen on 25.02.20.
+//  Copyright © 2020 Cedric Zwahlen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KlimaanlageRiemer_ZeroMotivationRunTests/KlimaanlageRiemer_ZeroMotivationRunTests.swift
@@ -0,0 +1,34 @@
+//
+//  KlimaanlageRiemer_ZeroMotivationRunTests.swift
+//  KlimaanlageRiemer_ZeroMotivationRunTests
+//
+//  Created by Cedric Zwahlen on 25.02.20.
+//  Copyright © 2020 Cedric Zwahlen. All rights reserved.
+//
+
+import XCTest
+@testable import KlimaanlageRiemer_ZeroMotivationRun
+
+class KlimaanlageRiemer_ZeroMotivationRunTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
